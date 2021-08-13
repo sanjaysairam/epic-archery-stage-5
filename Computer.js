19ea4a09e040f2babbdb9e1660b6e92455430e85
sanjaysairam <85401702+sanjaysairam@users.noreply.github.com>
2021-08-13 19:29:10 +0530

    Add files via upload

---
+++ Computer.js
@@ -0,0 +1,48 @@
+class Computer {
+  constructor(x, y, width, height) {
+    var options = {
+      isStatic: true
+    };
+
+    this.life1 = "green";
+
+    this.life2 = "green";
+
+    this.life3 = "green";
+
+    this.body = Bodies.rectangle(x, y, width, height, options);
+
+    this.width = width;
+    this.height = height;
+    this.image = loadImage("./assets/player.png");
+
+        World.add(world, this.body);
+  }
+
+   display() {
+    var pos = this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    imageMode(CENTER);
+    image(this.image, 0, 0, this.width, this.height);
+    pop();
+  }
+  life() {
+
+    push();
+    textSize(20);
+    fill("white");
+    text("computer",1100,40);
+
+    fill(this.life1);
+    rect(980, 50, 70, 30);
+    fill(this.life2);
+    rect(1050, 50, 70, 30);
+    fill(this.life3);
+    rect(1120, 50, 70, 30);
+    pop();
+
+  }
+}
